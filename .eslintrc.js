8b2a1fe68bc53337992d617302dcfda12cea5be6
CharaD7 <jijakahn6@gmail.com>
2022-11-22 19:32:12 +0000

    Added more eslint, prettier config and import sorter

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,49 +7,88 @@ module.exports = {
   extends: [
     'airbnb-typescript',
     'airbnb/hooks',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    '@react-native-community',
+    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  ignorePatterns: ['!.*', 'dist', 'node_modules'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: latest,
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['react', '@typescript-eslint', 'jest'],
+  plugins: ['react', 'react-native', 'jest', 'import'],
   rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
     'linebreak-style': 'off',
+    "max-lines": ["error", 80],
+    "no-extra-semi": ["error"],
     'prettier/prettier': [
       'error',
       {
         endOfLine: 'auto',
       },
     ],
+    quotes: ['error', 'double'],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "semi": [
       "error",
       "always",
       {
-        "omitLastInOneLineBlock": false
+        omitLastInOneLineBlock: false
       }
     ],
+    'sort-imports': ['error', {ignoreCase: true, ignoreDeclarationSort: true}],
   },
   "settings": {
     "import/parsers": {
       "@typescript-eslint/parser": [".ts", ".tsx"]
     },
     "import/resolver": {
-      "typescript": {}
+      typescript: true,
+      node: true
     },
     "react": {
-      "version": "detect"
+      version: "detect"
     }
   }
 };
