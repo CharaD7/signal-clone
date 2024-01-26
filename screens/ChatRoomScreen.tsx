aaefdf18bf4458c3f8b192aef79ee1a45d39628d
CharaD7 <jijakahn6@gmail.com>
2024-01-26 17:51:58 +0000

    Update FlatList parent view to use SafeAreaView

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,20 +1,22 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
 import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
 
 import chatRoomData from '~assets/dummy-data/Chats';
 import colors from '~components/Colors';
-import Message from '~components/Message/Message';
+import Message from '~components/Message';
+import MessageInput from '~components/MessageInput';
 
 const ChatRoomScreen = () => {
   return (
-    <View style={styles.page}>
+    <SafeAreaView style={styles.page}>
       <FlatList
         data={chatRoomData.messages}
         inverted
         renderItem={({ item }) => <Message message={item} />}
       />
-    </View>
+      <MessageInput />
+    </SafeAreaView>
   );
 };
 
