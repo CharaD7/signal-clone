c7c630983c9929f3a34a391e6d67bac7afa9a4b5
CharaD7 <jijakahn6@gmail.com>
2024-02-05 13:04:17 +0000

    Added graybg to ChatRoomItem style

--- components/ChatRoomItem/styles.ts
+++ components/ChatRoomItem/styles.ts
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
   container: {
-    backgroundColor: colors.white,
+    backgroundColor: colors.graybg,
     flexDirection: 'row',
     margin: 8,
   },
