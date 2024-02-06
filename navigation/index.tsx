bb85897857e0a1851da75f748edf711dcb86eb0f
CharaD7 <jijakahn6@gmail.com>
2024-02-06 15:14:48 +0000

    Introduced HomeHeader with icons and style

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -5,7 +5,7 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
+import { Feather, Ionicons } from '@expo/vector-icons';
 import {
   DarkTheme,
   DefaultTheme,
@@ -13,8 +13,16 @@ import {
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { ColorSchemeName, Image, Pressable, Text, View } from 'react-native';
+import {
+  ColorSchemeName,
+  Image,
+  Pressable,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 
+import Colors from '~components/Colors';
 import ChatRoomScreen from '~screens/ChatRoomScreen';
 import HomeScreen from '~screens/HomeScreen';
 import NotFoundScreen from '~screens/NotFoundScreen';
@@ -70,8 +78,15 @@ function RootNavigator() {
 const HomeHeader = (props) => {
   return (
     <View style={styles.container}>
-      <Image />
-      <Text>Home</Text>
+      <Image
+        source={{ uri: 'https://i.pravatar.cc/300' }}
+        style={styles.image}
+      />
+      <Text style={styles.text}>Signal</Text>
+      <View style={styles.icons}>
+        <Ionicons name="camera" size={24} color={Colors.blackTint} />
+        <Feather name="edit-2" size={24} color={Colors.blackTint} />
+      </View>
     </View>
   );
 };
--- navigation/styles.ts
+++ navigation/styles.ts
@@ -3,6 +3,26 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    alignItems: 'center',
+  },
+  image: {
+    width: 30,
+    height: 30,
+    borderRadius: 25,
+    right: 15,
+  },
+  text: {
+    flex: 1,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  icons: {
+    flexDirection: 'row',
+    width: 60,
+    right: 15,
+    justifyContent: 'space-between',
   },
 });
 
