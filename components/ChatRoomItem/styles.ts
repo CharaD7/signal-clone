c2746c1a154d92fd154fcf7791cb63d1bd561c57
CharaD7 <jijakahn6@gmail.com>
2024-02-07 22:09:38 +0000

    Disabled background picture on ChatItem

--- components/ChatRoomItem/styles.ts
+++ components/ChatRoomItem/styles.ts
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
   container: {
-    backgroundColor: colors.graybg,
+    // backgroundColor: colors.graybg,
     flexDirection: 'row',
     margin: 8,
   },
