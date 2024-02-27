d11e70d9d679f03a90c6a55f74ad1d497d1360b2
CharaD7 <jijakahn6@gmail.com>
2024-02-27 19:38:09 +0000

    Removed duplicate styles

--- navigation/styles.ts
+++ navigation/styles.ts
@@ -7,19 +7,6 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: 'center',
   },
-  roomHeaderContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-  },
-  roomHeaderImage: {
-    width: 30,
-    height: 30,
-    borderRadius: 25,
-    right: 35,
-  },
   image: {
     width: 30,
     height: 30,
@@ -30,12 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     fontWeight: 'bold',
     textAlign: 'center',
-  },
-  roomHeaderIcons: {
-    flexDirection: 'row',
-    width: 60,
-    right: 15,
-    justifyContent: 'space-between',
   },
   icons: {
     flexDirection: 'row',
